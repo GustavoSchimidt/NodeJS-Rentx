f3da0c45b5af400364c04153fdf4a270c015cb21
Gustavo Schimidt <schmidtgustavo7@gmail.com>
2021-08-31 09:35:32 -0400

    Imports da aplicaçao com paths

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import  multer  from "multer";
-import { CreateUserController } from "../modules/accounts/UseCases/createUser/CreateUserController";
-import { UpdateUserAvatarController } from "../modules/accounts/UseCases/updateUserAvatar/UpdateUserAvatarController";
-import uploadConfig from "../config/upload";
+
+import { CreateUserController } from "@modules/accounts/UseCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "@modules/accounts/UseCases/updateUserAvatar/UpdateUserAvatarController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import uploadConfig from "@config/upload";
 
 
 const usersRoutes = Router();
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,10 +1,12 @@
 import { container } from 'tsyringe';
-import { UsersRepository } from '../../modules/accounts/repositories/implementations/UsersRepository';
-import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
-import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
-import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository'
-import { SpecificationRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
-import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
+
+import { UsersRepository } from '@modules/accounts/repositories/implementations/UsersRepository';
+import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
+import { CategoriesRepository } from '@modules/cars/repositories/implementations/CategoriesRepository';
+import { SpecificationRepository } from '@modules/cars/repositories/implementations/SpecificationsRepository';
+import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository';
+
 
 //ICategoriesRepository
 container.registerSingleton<ICategoriesRepository>(
